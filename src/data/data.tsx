c58554f770e32ad42b1bb44f98a2cb423acdbb85
Thrilla1 <harrymi2003@gmail.com>
2024-05-08 02:10:11 +0100

    Initial version

---
+++ src/data/data.tsx
@@ -0,0 +1,293 @@
+import {
+  AcademicCapIcon,
+  CalendarIcon,
+  DownloadIcon,
+  FlagIcon,
+  MapIcon,
+  OfficeBuildingIcon,
+  SparklesIcon,
+} from '@heroicons/react/outline';
+
+import GithubIcon from '../components/Icon/GithubIcon';
+import InstagramIcon from '../components/Icon/InstagramIcon';
+import LinkedInIcon from '../components/Icon/LinkedInIcon';
+import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
+import TwitterIcon from '../components/Icon/TwitterIcon';
+import heroImage from '../images/header-background.webp';
+import profilepic from '../images/profilepic.jpg';
+import testimonialImage from '../images/testimonial.webp';
+import {
+  About,
+  ContactSection,
+  ContactType,
+  Hero,
+  HomepageMeta,
+  PortfolioItem,
+  SkillGroup,
+  Social,
+  TestimonialSection,
+  TimelineItem,
+} from './dataDef';
+
+/**
+ * Page meta data
+ */
+export const homePageMeta: HomepageMeta = {
+  title: 'Harry Michael-Iacovou',
+  description: ``,
+  ogImageUrl: ``,
+  twitterCardType: 'summary_large',
+  twitterSite: `@HarryIacovou`,
+  twitterCreator: `@HarryIacovou`,
+  twitterDomain: `reactresume.com`,
+  twitterUrl: `https://reactresume.com`,
+};
+
+/**
+ * Section definition
+ */
+export const SectionId = {
+  Hero: 'hero',
+  About: 'about',
+  Contact: 'contact',
+  Portfolio: 'portfolio',
+  Resume: 'resume',
+  Skills: 'skills',
+  Stats: 'stats'
+} as const;
+
+export type SectionId = typeof SectionId[keyof typeof SectionId];
+
+/**
+ * Hero section
+ */
+export const heroData: Hero = {
+  imageSrc: heroImage,
+  name: `Hi, I'm Harry Michael-Iacovou!`,
+  description: (
+    <>
+      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+        I'm a London based <strong className="text-stone-100">Undergraduate Computer Science student</strong> at <strong className="text-stone-100">Lancaster University</strong>, on track
+        to graduate with a first.
+      </p>
+      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+      </p>
+    </>
+  ),
+  actions: [
+    {
+      href: '/assets/resume.pdf',
+      text: 'Resume',
+      primary: true,
+      Icon: DownloadIcon,
+    },
+    {
+      href: `#${SectionId.Contact}`,
+      text: 'Contact',
+      primary: false,
+    },
+  ],
+};
+
+/**
+ * About section
+ */
+export const aboutData: About = {
+  profileImageSrc: profilepic,
+  description: `An ambitious final-year BSc Hons Computer Science student at Lancaster University, with a keen interest in Software Development. A team worker, as well as being adaptable to changing and fast paced environments. Meets deadlines, and always strives to make work that stands out.
+I am seeking opportunities in London and potentially internationally, working in full-stack development.`,
+  aboutItems: [
+    {label: 'Location', text: 'London, UK', Icon: MapIcon},
+    {label: 'Age', text: '20', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'British / Cypriot', Icon: FlagIcon},
+    {label: 'Interests', text: 'Traveling', Icon: SparklesIcon},
+    {label: 'Study', text: 'Lancaster University', Icon: AcademicCapIcon},
+    /*{label: 'Employment', text: 'Instant Domains, inc.', Icon: OfficeBuildingIcon},*/
+  ],
+};
+
+/**
+ * Skills section
+ */
+export const skills: SkillGroup[] = [
+  {
+    name: 'Spoken languages',
+    skills: [
+      {
+        name: 'English',
+        level: 10,
+      },
+      {
+        name: 'Greek',
+        level: 2,
+      },
+    ],
+  },
+  {
+    name: 'Frontend development',
+    skills: [
+      {
+        name: 'React',
+        level: 7,
+      },
+      {
+        name: 'Typescript',
+        level: 7,
+      },
+      {
+        name: 'HTML',
+        level: 10,
+      },
+	{
+        name: 'CSS (Tailwind, Standard)',
+        level: 7,
+      },
+    ],
+  },
+  {
+    name: 'Backend development',
+    skills: [
+      {
+        name: 'Java',
+        level: 8,
+      },
+      {
+        name: 'Python',
+        level: 8,
+      },
+	{
+        name: 'C',
+        level: 6,
+      },
+      {
+        name: 'C#',
+        level: 5,
+      },
+      	{
+        name: 'SQL (SQLite, MSSQL)',
+        level: 8,
+      },
+    ],
+  },
+];
+
+/**
+ * Portfolio section
+ */
+export const portfolioItems: PortfolioItem[] = [
+  {
+    title: 'Project title',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    imageUrl: 'https://source.unsplash.com/random/',
+  },
+];
+
+/**
+ * Resume section -- TODO: Standardize resume contact format or offer MDX
+ */
+export const education: TimelineItem[] = [
+  {
+    date: 'Oct 2021 - Jul 2024',
+    location: 'Lancaster University',
+    title: 'BSc (Hons) Computer Science',
+    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+  },
+  {
+    date: '2018 - 2021',
+    location: 'Langley Park School for Boys',
+    title: 'A-Levels in Computer Science, Politics & Media Studies',
+    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+  },
+];
+
+export const experience: TimelineItem[] = [
+  {
+    date: 'March 2010 - Present',
+    location: 'Awesome Development Company',
+    title: 'Senior UX Engineer',
+    content: (
+      <p>
+        Describe work, special projects, notable achievements, what technologies you have been working with, and
+        anything else that would be useful for an employer to know.
+      </p>
+    ),
+  },
+  {
+    date: 'March 2007 - February 2010',
+    location: 'Garage Startup Studio',
+    title: 'Junior bug fixer',
+    content: (
+      <p>
+        Describe work, special projects, notable achievements, what technologies you have been working with, and
+        anything else that would be useful for an employer to know.
+      </p>
+    ),
+  },
+];
+
+/**
+ * Testimonial section
+ */
+export const testimonial: TestimonialSection = {
+  imageSrc: testimonialImage,
+  testimonials: [
+    {
+      name: 'John Doe',
+      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
+    },
+    {
+      name: 'Jane Doe',
+      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
+    },
+    {
+      name: 'Someone else',
+      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
+      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
+    },
+  ],
+};
+
+/**
+ * Contact section
+ */
+
+export const contact: ContactSection = {
+  headerText: 'Get in touch.',
+  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  items: [
+    {
+      type: ContactType.Email,
+      text: 'harry@harrymi.co.uk',
+      href: 'mailto:harry@harrymi.co.uk',
+    },
+    {
+      type: ContactType.Location,
+      text: 'London, UK',
+      href: '',
+    },
+    {
+      type: ContactType.Instagram,
+      text: '@harrymi03',
+      href: 'https://www.instagram.com/harrymi03/',
+    },
+    {
+      type: ContactType.Github,
+      text: 'Thrilla1',
+      href: 'https://github.com/Thrilla1',
+    },
+  ],
+};
+
+/**
+ * Social items
+ */
+export const socialLinks: Social[] = [
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/Thrilla1'},
+  /*{label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},*/
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/harry-iacovou/'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/harrymi03/'},
+  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/HarryIacovou'},
+];
