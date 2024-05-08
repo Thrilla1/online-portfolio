ddff819222547d54d804862caeced8b65da04845
Thrilla1 <harrymi2003@gmail.com>
2024-05-08 03:15:09 +0100

    Updates

---
+++ _next/static/PzifVCx1MpJiIUSTbF1cA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/963-c81a26ba76a64bac.js","static/chunks/pages/index-b685635311f9b68f.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
